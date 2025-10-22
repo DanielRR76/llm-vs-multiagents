69f4d8b8dc400c5bbd529f7890a467f0eefc7756
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2025-10-22 20:53:05 -0300

    feat: Refactor AgentManager and Main to integrate CodeAnalyzer and TestStrategist agents

--- src/application/agent_manager.py
+++ src/application/agent_manager.py
@@ -2,25 +2,31 @@
 from langgraph.graph import END, START, StateGraph
 
 from src.domain.state import State
-from src.application.quality_agent import QualityAgent
-from src.application.coverage_agent import CoverageAgent
+from src.application.coverage_analyzer import CoverageAnalyzer
 from src.application.mutation_test_agent import MutationTestAgent
-from src.application.unit_test_agent import UnitTestAgent
+from src.application.test_generator import TestGenerator
+from src.application.quality_agent import QualityAgent
+from src.application.code_analyzer import CodeAnalyzer
+from src.application.test_strategist import TestStrategist
 
 
 class AgentManager:
 
     def __init__(
         self,
-        coverage_agent: CoverageAgent,
+        coverage_agent: CoverageAnalyzer,
         mutation_test_agent: MutationTestAgent,
         quality_agent: QualityAgent,
-        unit_test_agent: UnitTestAgent,
+        unit_test_agent: TestGenerator,
+        code_analyzer_agent: CodeAnalyzer,
+        test_strategist_agent: TestStrategist,
     ):
         self.coverage_agent = coverage_agent
         self.mutation_test_agent = mutation_test_agent
-        self.quality_agent = quality_agent
         self.unit_test_agent = unit_test_agent
+        self.code_analyzer_agent = code_analyzer_agent
+        self.test_strategist_agent = test_strategist_agent
+        self.quality_agent = quality_agent
         self.workflow = StateGraph(State)
         self._build_workflow()
         self.chain = self.workflow.compile()
@@ -29,3 +35,5 @@ class AgentManager:
         self.workflow.add_node("test_coverage", self.coverage_agent.respond)
         self.workflow.add_node("mutation_testing", self.mutation_test_agent.respond)
         self.workflow.add_node("unit_test", self.unit_test_agent.respond)
+        self.workflow.add_node("code_analysis", self.code_analyzer_agent.respond)
+        self.workflow.add_node("test_strategy", self.test_strategist_agent.respond)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-import json
 import logging
-from typing import Any, Dict
 
 from config.config import Config
 from src.application.agent_manager import AgentManager
-from src.application.coverage_agent import CoverageAgent
+from src.application.coverage_analyzer import CoverageAnalyzer
 from src.application.mutation_test_agent import MutationTestAgent
 from src.application.quality_agent import QualityAgent
-from src.application.unit_test_agent import UnitTestAgent
+from src.application.test_generator import TestGenerator
 from src.application.query_manager import QueryManager
 from src.infrastructure.ai_agents import Agents
 from src.interfaces.ui.stremlit_app import Index
+from src.application.code_analyzer import CodeAnalyzer
+from src.application.test_strategist import TestStrategist
 
 
 class Main:
@@ -20,16 +20,22 @@ class Main:
 
     def run(self):
         try:
-            coverage_agent = CoverageAgent(
+            coverage_agent = CoverageAnalyzer(
                 Agents.load_agent(*self.config.coverage_agent)
             )
             mutation_test_agent = MutationTestAgent(
                 Agents.load_agent(*self.config.mutation_test_agent)
             )
-            quality_agent = QualityAgent(Agents.load_agent(*self.config.quality_agent))
-            unit_test_agent = UnitTestAgent(
+            unit_test_agent = TestGenerator(
                 Agents.load_agent(*self.config.unit_test_agent)
             )
+            code_analyzer_agent = CodeAnalyzer(
+                Agents.load_agent(*self.config.code_analyzer_agent)
+            )
+            test_strategist_agent = TestStrategist(
+                Agents.load_agent(*self.config.test_strategist_agent)
+            )
+            quality_agent = QualityAgent(Agents.load_agent(*self.config.quality_agent))
 
             logging.info("Todos os agentes carregados com sucesso")
         except Exception as e:
@@ -38,8 +44,10 @@ class Main:
         agent_manager = AgentManager(
             coverage_agent=coverage_agent,
             mutation_test_agent=mutation_test_agent,
-            quality_agent=quality_agent,
             unit_test_agent=unit_test_agent,
+            quality_agent=quality_agent,
+            code_analyzer_agent=code_analyzer_agent,
+            test_strategist_agent=test_strategist_agent,
         )
 
         query_manager = QueryManager(agent_manager=agent_manager)
