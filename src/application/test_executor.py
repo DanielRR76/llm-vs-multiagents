6a7d8342e6282cf61945f7df2a3eb067b5a53b6f
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2025-10-24 09:57:18 -0300

    feat: Implement test execution functionality in TestExecutor class

--- src/application/test_executor.py
+++ src/application/test_executor.py
@@ -1,7 +1,20 @@
+import subprocess
 from src.domain.state import State
+from src.utils.file_manager import FileManager
 
 
 class TestExecutor:
 
     def execute(self, state: State):
-        return {"code_response": state}  # to do
+        test_file_path = "src/environment/python/input_code_test.py"
+        FileManager.writeFile(test_file_path, state.test_generator_response)
+
+        command = ["make", "python-test"]
+
+        result = subprocess.run(command, capture_output=True, text=True)
+        response = {
+            "stdout": result.stdout,
+            "stderr": result.stderr,
+            "returncode": result.returncode,
+        }
+        return {"test_executor_response": response}
