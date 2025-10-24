d1ca664554e71a2f5720436f79162577985c3a8d
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2025-10-24 09:52:21 -0300

    feat: Update State class to include test generator and executor responses

--- src/domain/state.py
+++ src/domain/state.py
@@ -8,6 +8,7 @@ class State:
     coverage_agent_response: str = ""
     mutation_agent_response: str = ""
     performance_agent_response: str = ""
-    unit_agent_response: str = ""
+    test_generator_response: str = ""
+    test_executor_response: Dict[str, object] = field(default_factory=dict)
     quality_agent_response: str = ""
     final_code: str = ""
