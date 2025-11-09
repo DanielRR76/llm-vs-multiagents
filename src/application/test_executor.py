ec646852823656c064fbc56d9011a6c2ad2b8ca8
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2025-11-09 14:56:25 -0300

    feat: Add execute_mutation_test method to TestExecutor for mutation test execution

--- src/application/test_executor.py
+++ src/application/test_executor.py
@@ -1,6 +1,7 @@
 import subprocess
 from src.domain.state import State
 from src.utils.file_manager import FileManager
+import re
 
 
 class TestExecutor:
@@ -24,3 +25,23 @@ class TestExecutor:
         if logs["returncode"] != 0:
             return False
         return True
+
+    def execute_mutation_test(self, state: State):
+        mutation_code: str = str(state.mutation_agent_response)
+        FileManager.writeFile("src/environment/python/input_code.py", mutation_code)
+
+        command = ["make", "python-test"]
+
+        result = subprocess.run(command, capture_output=True, text=True)
+        response = {
+            "stdout": result.stdout,
+            "stderr": result.stderr,
+            "returncode": result.returncode,
+        }
+        pattern = (
+            r"(=+ short test summary info =+\n[\s\S]+?=+\s*\d+\sfailed,.*?passed.*?=+)"
+        )
+        match = re.search(pattern, result.stdout)
+        if match:
+            response["stdout"] = match.group(1)
+        return {"test_executor_mutation_response": response.get("stdout")}
