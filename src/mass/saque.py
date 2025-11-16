2c721be5cf2d73cd13bb25211016cc8aa9c6fa0a
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2025-11-16 17:35:56 -0300

    refactor: Remove static method decorator from validar_saque and clean up formatting

--- src/mass/saque.py
+++ src/mass/saque.py
@@ -1,14 +1,14 @@
 from decimal import Decimal
 from datetime import time
 
+
 class SaqueUC:
 
-    @staticmethod
-    def validar_saque(user_id: int, valor: Decimal, agora, account_dao):
+    def validar_saque(self, user_id: int, valor: Decimal, agora, account_dao):
         bloqueio_inicio1 = time(12, 0)
-        bloqueio_fim1    = time(12, 30)
+        bloqueio_fim1 = time(12, 30)
         bloqueio_inicio2 = time(18, 0)
-        bloqueio_fim2    = time(18, 30)
+        bloqueio_fim2 = time(18, 30)
 
         conta = account_dao.get_by_user_id(user_id)
 
